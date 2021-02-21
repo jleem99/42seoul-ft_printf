8e0b5a5ea6985dfa99f816d5d8cbad250344981c
kch79624 <kch79624@gmail.com>
2021-02-21 09:13:11 +0900

    Update main.c: Test examples

--- main.c
+++ main.c
@@ -10,7 +10,18 @@ int main(void)
 	// '%o' > '0'
 	// '+' vs '%o'
 	// ' ' vs '%o'
-	printf("%d\n", ft_printf("hello%world", 1234, 1234));
-	printf("%++++++++++0+#40.*f", 10, 1234.5678);
+	// printf("\n%d\n", ft_printf("he%c", 'a'));
+	// printf("%++++++++++0+#40.*f", 10, 1234.5678);
+	// printf("%.g", 1234.234239487);
+	ft_printf("Characters: %c %c \n", 'a', 65);
+	ft_printf("Decimals: %d %ld\n", -1977, 650000L);
+	ft_printf("Preceding with blanks: %10d \n", 1977);
+	ft_printf("Preceding with zeros: %010d \n", 1977);
+	ft_printf("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
+	ft_printf("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
+	ft_printf("Width trick: %*d \n", 5, 10);
+	ft_printf("Percent sign: %%\n");
+	ft_printf("%020s \n", "A string");
+	printf("\n\n%020s", "hello world");
 	return (0);
 }
