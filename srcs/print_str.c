6152743bc0cbbfe711b179fb13237caaa5cd0dd9
jleem <kch79624@gmail.com>
2021-02-23 16:46:14 +0900

    Update print_str.c

--- srcs/print_str.c
+++ srcs/print_str.c
@@ -6,7 +6,7 @@
 /*   By: jleem <jleem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 22:48:23 by jleem             #+#    #+#             */
-/*   Updated: 2021/02/20 22:49:14 by jleem            ###   ########.fr       */
+/*   Updated: 2021/02/22 17:56:32 by jleem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@ void	print_str(t_printer *printer, t_specifier *specifier) // validate malloc
 	char	*str;
 
 	str = va_arg(*printer->ap, char *);
+	if (!str)
+		str = "(null)";
 	print(str, printer, specifier);
 }
