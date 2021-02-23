2cd1f6117849ccdcd70d592c730aab42792e9e78
jleem <kch79624@gmail.com>
2021-02-23 16:48:34 +0900

    Add print_integer_utils

--- srcs/print_integer_utils2.c
+++ srcs/print_integer_utils2.c
@@ -1,25 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_integer_utils.c                              :+:      :+:    :+:   */
+/*   print_integer_utils2.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jleem <jleem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 07:43:49 by jleem             #+#    #+#             */
-/*   Updated: 2021/02/22 17:02:34 by jleem            ###   ########.fr       */
+/*   Updated: 2021/02/22 17:03:14 by jleem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "print.h"
 #include "libft.h"
 
-char		*ft_imtoa_10(intmax_t n)
+char		*ft_uimtoa_10(uintmax_t n)
 {
 	char		digits[40];
 	char		digits_rev[39];
 	int			i;
 	int			i_rev;
-	int const	minus = (n < 0);
 
 	if (n == 0)
 		return (ft_strdup("0"));
@@ -27,28 +26,21 @@ char		*ft_imtoa_10(intmax_t n)
 	i_rev = -1;
 	while (n != 0)
 	{
-		if (minus)
-			digits_rev[++i_rev] = '0' - (n % 10);
-		else
-			digits_rev[++i_rev] = '0' + (n % 10);
+		digits_rev[++i_rev] = '0' + (n % 10);
 		n /= 10;
 	}
 	while (i_rev >= 0)
 		digits[i++] = digits_rev[i_rev--];
 	digits[i] = '\0';
-	if (minus)
-		return (ft_strjoin("-", digits));
-	else
-		return (ft_strdup(digits));
+	return (ft_strdup(digits));
 }
 
-char		*ft_imtoa_8(intmax_t n)
+char		*ft_uimtoa_8(uintmax_t n)
 {
 	char		digits[44];
 	char		digits_rev[43];
 	int			i;
 	int			i_rev;
-	int const	minus = (n < 0);
 
 	if (n == 0)
 		return (ft_strdup("0"));
@@ -56,28 +48,21 @@ char		*ft_imtoa_8(intmax_t n)
 	i_rev = -1;
 	while (n != 0)
 	{
-		if (minus)
-			digits_rev[++i_rev] = '0' - (n % 8);
-		else
-			digits_rev[++i_rev] = '0' + (n % 8);
+		digits_rev[++i_rev] = '0' + (n % 8);
 		n /= 8;
 	}
 	while (i_rev >= 0)
 		digits[i++] = digits_rev[i_rev--];
 	digits[i] = '\0';
-	if (minus)
-		return (ft_strjoin("-", digits));
-	else
-		return (ft_strdup(digits));
+	return (ft_strdup(digits));
 }
 
-char		*ft_imtoa_16(intmax_t n, char base)
+char		*ft_uimtoa_16(uintmax_t n, char base)
 {
 	char		digits[33];
 	char		digits_rev[32];
 	int			i;
 	int			i_rev;
-	int const	minus = (n < 0);
 
 	if (n == 0)
 		return (ft_strdup("0"));
@@ -85,27 +70,14 @@ char		*ft_imtoa_16(intmax_t n, char base)
 	i_rev = -1;
 	while (n != 0)
 	{
-		if (minus)
-		{
-			if (n % 16 < 10)
-				digits_rev[++i_rev] = '0' - (n % 16);
-			else
-				digits_rev[++i_rev] = base - (n % 16 - 10);
-		}
+		if (n % 16 < 10)
+			digits_rev[++i_rev] = '0' + (n % 16);
 		else
-		{
-			if (n % 16 < 10)
-				digits_rev[++i_rev] = '0' + (n % 16);
-			else
-				digits_rev[++i_rev] = base + (n % 16 - 10);
-		}
+			digits_rev[++i_rev] = base + (n % 16 - 10);
 		n /= 16;
 	}
 	while (i_rev >= 0)
 		digits[i++] = digits_rev[i_rev--];
 	digits[i] = '\0';
-	if (minus)
-		return (ft_strjoin("-", digits));
-	else
-		return (ft_strdup(digits));
+	return (ft_strdup(digits));
 }
