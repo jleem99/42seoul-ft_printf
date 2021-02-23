83327e4e2685bf18d970386aca44bddfd3e1bbe2
jleem <kch79624@gmail.com>
2021-02-23 16:45:47 +0900

    Update print_ptr.c

--- srcs/print_ptr.c
+++ srcs/print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: jleem <jleem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 22:50:58 by jleem             #+#    #+#             */
-/*   Updated: 2021/02/20 22:51:07 by jleem            ###   ########.fr       */
+/*   Updated: 2021/02/22 17:55:15 by jleem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,19 @@
 void	print_ptr(t_printer *printer, t_specifier *specifier) // validate malloc
 {
 	char	*str;
+	void	*ptr;
 
-	str = "[ptr]";
-	print(str, printer, specifier);
+	ptr = va_arg(*printer->ap, void *);
+	if (ptr)
+	{
+		str = ft_uimtoa_16(ptr, 'a');
+		specifier->f_pound = 1;
+		apply_flag_integer(&str, specifier, 1);
+		print(str, printer, specifier);
+		free(str);
+	}
+	else
+	{
+		print("(nil)", printer, specifier);
+	}
 }
