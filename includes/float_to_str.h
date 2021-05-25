3bce70892282ab3b9db82f2ded10a2967208a32a
jleem <kch79624@gmail.com>
2021-05-25 17:08:14 +0900

    Bigint: Use uint32_t instead of t_size

--- includes/float_to_str.h
+++ includes/float_to_str.h
@@ -6,7 +6,7 @@
 /*   By: jleem <jleem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 03:24:23 by jleem             #+#    #+#             */
-/*   Updated: 2021/05/23 17:10:29 by jleem            ###   ########.fr       */
+/*   Updated: 2021/05/25 05:42:21 by jleem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,59 +21,59 @@
 #  include <machine/endian.h>
 # endif
 
-typedef unsigned int	t_size;
+# include <stdint.h>
 
-typedef union	u_ieee754_double
+typedef union		u_ieee754_double
 {
-	double		d;
+	double			d;
 
 	struct
 	{
-		t_size	mantissa1:32;
-		t_size	mantissa0:20;
-		t_size	exponent:11;
-		t_size	negative:1;
-	}			ieee;
+		uint32_t	mantissa1:32;
+		uint32_t	mantissa0:20;
+		uint32_t	exponent:11;
+		uint32_t	negative:1;
+	}				ieee;
 
 	struct
 	{
-		t_size	mantissa1:32;
-		t_size	mantissa0:19;
-		t_size	quiet_nan:1;
-		t_size	exponent:11;
-		t_size	negative:1;
-	}			ieee_nan;
-}				t_ieee754;
+		uint32_t	mantissa1:32;
+		uint32_t	mantissa0:19;
+		uint32_t	quiet_nan:1;
+		uint32_t	exponent:11;
+		uint32_t	negative:1;
+	}				ieee_nan;
+}					t_ieee754;
 
 # define IEEE754_DOUBLE_BIAS 0x3ff
 
-typedef union	u_ieee854_long_double
+typedef union		u_ieee854_long_double
 {
-	long double	d;
+	long double		d;
 
 	struct
 	{
-		t_size	mantissa1:32;
-		t_size	mantissa0:32;
-		t_size	exponent:15;
-		t_size	negative:1;
-		t_size	empty:16;
-	}			ieee;
+		uint32_t	mantissa1:32;
+		uint32_t	mantissa0:32;
+		uint32_t	exponent:15;
+		uint32_t	negative:1;
+		uint32_t	empty:16;
+	}				ieee;
 
 	struct
 	{
-		t_size	mantissa1:32;
-		t_size	mantissa0:30;
-		t_size	quiet_nan:1;
-		t_size	one:1;
-		t_size	exponent:15;
-		t_size	negative:1;
-		t_size	empty:16;
-	}			ieee_nan;
-}				t_ieee854;
+		uint32_t	mantissa1:32;
+		uint32_t	mantissa0:30;
+		uint32_t	quiet_nan:1;
+		uint32_t	one:1;
+		uint32_t	exponent:15;
+		uint32_t	negative:1;
+		uint32_t	empty:16;
+	}				ieee_nan;
+}					t_ieee854;
 
 # define IEEE854_LONG_DOUBLE_BIAS 0x3fff
 
-char			*long_double_to_str_10(long double flt, int precision);
+char				*long_double_to_str_10(long double flt, int precision);
 
 #endif
