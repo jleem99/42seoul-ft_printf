8ad85b8c1a73f8bae2e09d08ba0feec798cc4c0d
jleem <kch79624@gmail.com>
2021-05-29 02:31:32 +0900

    Use MACRO for specifier

--- main.c
+++ main.c
@@ -48,80 +48,75 @@ void	inspect_bit(t_itype val)
 #include "libft_bonus.h"
 #include "convert.h"
 #include "specifier.h"
+#define TEST_SPECIFIER "f"
+
 int main(void)
 {
 	INIT_TEST();
 
-	// long double num = -11234.5891727419283478912371465745674567456234123L;
-	// TEST("%.70Lf", num);
-	// TEST("%Lf", num);
-	// TEST("%8.Lf", num);
-	// TEST("%#8.Lf", num);
-	// TEST("%+#8.Lf", num);
-	// TEST("% .Lf", num);
-	// TEST("%+ .Lf", num);
-	// TEST("%.70Lf", -num);
-	// TEST("%Lf", -num);
-	// TEST("%8.Lf", -num);
-	// TEST("%#8.Lf", -num);
-	// TEST("%+#8.Lf", -num);
-	// TEST("% .Lf", -num);
-	// TEST("%+ .Lf", -num);
-	// float numf = -11234.5891727419283478912371465745674567456234123f;
-	// TEST("%.70f", numf);
-	// TEST("%f", numf);
-	// TEST("%8.f", numf);
-	// TEST("%#8.f", numf);
-	// TEST("%+#8.f", numf);
-	// TEST("% .f", numf);
-	// TEST("%+ .f", numf);
-	// TEST("%.70f", -numf);
-	// TEST("%f", -numf);
-	// TEST("%8.f", -numf);
-	// TEST("%#8.f", -numf);
-	// TEST("%+#8.f", -numf);
-	// TEST("% .f", -numf);
-	// TEST("%+ .f", -numf);
+	long double num = -11234.5891727419283478912371465745674567456234123L;
+	float numf = -11234.5891727419283478912371465745674567456234123f;
 
-	// test("%1f", -1.5);
-	// test("%1f", -1.05);
-	// test("%1f", -1.005);
-	// test("%1f", -1.0625);
-	// test("%1f", -14353453453423.899999999999999999999999999999999);
-	// test("%1f", -14353453453423.0899999999999999999999999999999999);
-	// test("%.0f", 0.5);
-	// test("%.0f", 1.5);
-	// test("%.0f", -0.5);
-	// test("%.0f", -1.5);
-
-
-	// test("% 0-7.1f", 7.3);
-	// test("% 0#7.1f", 7.3);
-	// test("%0#7.1f", 7.3);
-	// // test("% 0#-05.3f", -0.0); // timeout
-	// // test("% 0#-05.3f", 0.0); // timeout
-	// test("%.10f", 23.000000041);
-	// test("%.12f", 1.025978541236587568);
-	// test("% +.3f", -7.3);
-	// test("% 05.0f", -7.3);
-	// test("%.1f", -3.85);
-	// sizeof(long double);
-	// test("%.f", 0.4);
-	// test("%.f", 0.5);
-	// test("%.f", 0.6);
-	// test("%.f", -0.4);
-	// test("%.f", -0.5);
-	// // test("%#Lf", 10e100000L);
-
-	// test("%.f", 2.50000000001);
-	// test("%.f", -0.6);
-
-	double special;
-	*((unsigned long *)&special) = 0x7FF0000000100000;
-	// test("%+6f", 0.0/0.0);
-	test("% F", 0.0/0.0);
-	// test("% .7f", 0.0/0.0);
-
+	TEST("%.70L"TEST_SPECIFIER, num);
+	TEST("%L"TEST_SPECIFIER, num);
+	TEST("%8.L"TEST_SPECIFIER, num);
+	TEST("%#8.L"TEST_SPECIFIER, num);
+	TEST("%+#8.L"TEST_SPECIFIER, num);
+	TEST("% .L"TEST_SPECIFIER, num);
+	TEST("%+ .L"TEST_SPECIFIER, num);
+	TEST("%.70L"TEST_SPECIFIER, -num);
+	TEST("%L"TEST_SPECIFIER, -num);
+	TEST("%8.L"TEST_SPECIFIER, -num);
+	TEST("%#8.L"TEST_SPECIFIER, -num);
+	TEST("%+#8.L"TEST_SPECIFIER, -num);
+	TEST("% .L"TEST_SPECIFIER, -num);
+	TEST("%+ .L"TEST_SPECIFIER, -num);
+	TEST("%.70"TEST_SPECIFIER, numf);
+	TEST("%"TEST_SPECIFIER, numf);
+	TEST("%8."TEST_SPECIFIER, numf);
+	TEST("%#8."TEST_SPECIFIER, numf);
+	TEST("%+#8."TEST_SPECIFIER, numf);
+	TEST("% ."TEST_SPECIFIER, numf);
+	TEST("%+ ."TEST_SPECIFIER, numf);
+	TEST("%.70"TEST_SPECIFIER, -numf);
+	TEST("%"TEST_SPECIFIER, -numf);
+	TEST("%8."TEST_SPECIFIER, -numf);
+	TEST("%#8."TEST_SPECIFIER, -numf);
+	TEST("%+#8."TEST_SPECIFIER, -numf);
+	TEST("% ."TEST_SPECIFIER, -numf);
+	TEST("%+ ."TEST_SPECIFIER, -numf);
+	test("%1"TEST_SPECIFIER, -1.5);
+	test("%1"TEST_SPECIFIER, -1.05);
+	test("%1"TEST_SPECIFIER, -1.005);
+	test("%1"TEST_SPECIFIER, -1.0625);
+	test("%1"TEST_SPECIFIER, -14353453453423.899999999999999999999999999999999);
+	test("%1"TEST_SPECIFIER, -14353453453423.0899999999999999999999999999999999);
+	test("%.0"TEST_SPECIFIER, 0.5);
+	test("%.0"TEST_SPECIFIER, 1.5);
+	test("%.0"TEST_SPECIFIER, -0.5);
+	test("%.0"TEST_SPECIFIER, -1.5);
+	test("% 0-7.1"TEST_SPECIFIER, 7.3);
+	test("% 0#7.1"TEST_SPECIFIER, 7.3);
+	test("%0#7.1"TEST_SPECIFIER, 7.3);
+	// test("% 0#-05.3"TEST_SPECIFIER, -0.0); // timeout
+	// test("% 0#-05.3"TEST_SPECIFIER, 0.0); // timeout
+	test("%.10"TEST_SPECIFIER, 23.000000041);
+	test("%.12"TEST_SPECIFIER, 1.025978541236587568);
+	test("% +.3"TEST_SPECIFIER, -7.3);
+	test("% 05.0"TEST_SPECIFIER, -7.3);
+	test("%.1"TEST_SPECIFIER, -3.85);
+	sizeof(long double);
+	test("%."TEST_SPECIFIER, 0.4);
+	test("%."TEST_SPECIFIER, 0.5);
+	test("%."TEST_SPECIFIER, 0.6);
+	test("%."TEST_SPECIFIER, -0.4);
+	test("%."TEST_SPECIFIER, -0.5);
+	// test("%#L"TEST_SPECIFIER, 10e100000L);
+	test("%."TEST_SPECIFIER, 2.50000000001);
+	test("%."TEST_SPECIFIER, -0.6);
+	test("%+6"TEST_SPECIFIER, 0.0/0.0);
+	test("% "TEST_SPECIFIER, 0.0/0.0);
+	// test("% .7"TEST_SPECIFIER, 0.0/0.0);
 	// TEST("%3c", 'a');
 	// TEST("%02%");
 	// TEST("%p", 0x123a);
@@ -129,11 +124,11 @@ int main(void)
 	// TEST("%.d", 0);
 	// TEST("%20.30p", 0x1234f);
 	// TEST("% +20.30q %d%d", 0x1234f, 1234, 4321);
-	// TEST("%f", 1234000000000000000000000000000000000000000000000000000000000000000000000000000.f);
-	// TEST("%f", __FLT_MAX__);
-	// TEST("%f", __FLT_MAX__ + 1.0f);
-	// TEST("%f", __FLT_MAX__ + 2.0f);
-	// test("%.*f", -3, 3.1415926535);
+	// TEST("%"TEST_SPECIFIER, 1234000000000000000000000000000000000000000000000000000000000000000000000000000.f);
+	// TEST("%"TEST_SPECIFIER, __FLT_MAX__);
+	// TEST("%"TEST_SPECIFIER, __FLT_MAX__ + 1.0f);
+	// TEST("%"TEST_SPECIFIER, __FLT_MAX__ + 2.0f);
+	// test("%.*"TEST_SPECIFIER, -3, 3.1415926535);
 
 	// printf("%d vs %d vs %d\n", sizeof(float), sizeof(double), sizeof(long double));
 	return (0);
